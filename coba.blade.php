a2171d7ef21d8795c0003b57a46109d0050cd8cd
Dwiiiiii <87762336+Dwiiiiii@users.noreply.github.com>
2022-05-19 10:46:43 +0700

    Add files via upload

---
+++ coba.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title','detail group')
+
+
+@section('content')
+    Urutan ke - {{ $ke }}
+@endsection
+
+    
---
+++ home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title' , 'UTS')
+
+@section('content')
+<div id="content" class="p-4 p-md-5 pt-5">
+    <h2 class="mb-4">UTS Laravel - Illal Dwi Sahputra </h2>
+
+<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <div class="container">
+        <div class="row">
+              <div class="col">
+            <card>
+              <h6>20200121010</h6>
+            </card>
+
+              </form>
+            </div>
+         </div>
+      </div>
+    </div>
+</div>
+
+
+@endsection
